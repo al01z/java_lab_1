c28f7515ba7b25ca589301ffe646791df74bda22
al01z <dampc.artur@gmail.com>
2021-09-25 09:31:43 +0200

    komentarz

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Witaj śwliescie!");
+        System.out.println("Witaj lśwliescie!");
         System.out.println("wwwwwwwwwwwwwwwww");
 
     }
