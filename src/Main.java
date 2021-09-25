9e60c7353bdbd39cbdfc562f98e888bef81efaad
al01z <dampc.artur@gmail.com>
2021-09-25 09:49:15 +0200

    komentarz

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-
+        System.out.println("To jest nowa linia tekstu w konsoli");
+        System.out.printf("Oto implementacja na bazie %s dla cwiczenia %d z zajeć.\n", "przykładu", 1);
+        System.out.print("to jest tekst zapisany w konsoli");
+        System.out.print(" bez znacznika nowej lini");
+        System.out.print(" przy pomocy print");
 
     }
 }
