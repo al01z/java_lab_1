00a0b2977dce787d5360611825fad7a691ac50ba
Al01z <dampc.artur@gmail.com>
2021-12-18 03:30:30 +0100

    dodanie cw 6

--- src/Main.java
+++ src/Main.java
@@ -4,38 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* ćw.5 */
+        /* Tablice - cw_6 */
 
-        /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
-         * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
-         * */
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Podaj wiek: ");
-        int wiek = scan.nextInt();
-        if ((wiek%3)==0) {
-            System.out.println("Podzielny przez 3");
-        } else {
-            System.out.println("Niepodzielny przez 3");
-        }
-
-
-        /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
-        System.out.print("Podaj numer indeksu: ");
-        int numerIndeksu = scan.nextInt();
-        String wynik = ((numerIndeksu%2)==0 ? "Parzysty" : "Nieparzysty");
-        System.out.println("Twoj numer indeksu jest: " + wynik);
-
-
-        /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
-         * i stwórz konstrukcję else if z dowolnymi komunikatami */
-        System.out.print("Podaj liczbe zmienno przecinkowa: ");
-        double liczbaZmiennoprzecinkowa = scan.nextDouble();
-        if ((liczbaZmiennoprzecinkowa%1)==0) {
-            System.out.println("To chyba jednak liczba calkowikita.");
-        } else if (liczbaZmiennoprzecinkowa > 10) {
-            System.out.println("Mamy tu liczbe wieksza od 10");
-        } else {
-            System.out.println("Oho mniejsza od 10. No prosze");
+        /* zad1. stworzyć tablicę typu double 5-cio elementową,
+        przypisać do niej wartości i wyświetlić */
+        double[] tab = new double[]{1.2, 2.21, 0.73, 1.304, 2.11};
+        for (int i = 0; i < tab.length; i++) {
+            System.out.print(tab[i] + " ");
         }
 
     }
